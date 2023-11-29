e59881c3b324a7e04508f8fef5bcbe83aea97c64
yannis.lemaire <yannis.lemaire@CP-SIO-W11-PG02.bts.sio>
2023-11-29 16:59:24 +0100

    commit creation de plusieurs routes

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -11,11 +11,24 @@ use App\Entity\Employe;
 class EmployeController extends AbstractController
 {
     #[Route('/employe/voirmieux/{id}', name: 'app_employe', requirements:["id"=>"\d+"], defaults: ['id'=>'99'])]
-    #[Route('/employe/voirnom/{nom}', name: 'app_employe', requirements:["id"=>"^\w[1,}"], defaults: ['nom'=>'benoit'])]
     public function VoirEmploye(ManagerRegistry $doctrine, int $id): Response
     {
         $employe=$doctrine->getRepository(Employe::class)->find($id);
         $titre = "Employé";
         return $this->render('employe/index.html.twig', compact('titre','employe'));
+    }
+    
+    #[Route('/employe/voirnom/{nom}', name: 'app_employe', requirements:["id"=>"^\w[1,}"], defaults: ['nom'=>'benoit'])]
+    public function VoirNom(ManagerRegistry $doctrine, string $nom): Response
+    {
+        $employe=$doctrine->getRepository(Employe::class)->find($nom);
+        $titre = $nom;
+        return $this->render('employe/index.html.twig', compact('titre','employe'));
+    }
+    
+    #[Route('/employe/voirnom/{nom}', name: 'app_employe', requirements:["id"=>"^\w[1,}"], defaults: ['nom'=>'benoit'])]
+    public function redirection(string $nom): Response
+    {
+        return $this->redirectToRoute("employe_voirnom", ['nom'=>"Bond"]);
     }
 }
